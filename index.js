1d20d8896970b5a9509d0205ede2435de2efd18f
Henry Chang, aka Li-Heng <lihengc6@gmail.com>
2024-03-15 19:29:17 -0400

    Update style.css and index.html

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// json server has to be in a different folder
+
+const EditIcon = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>`
+
+
+const DeleteIcon = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>`
+
+const SaveIcon = `<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21,20V8.414a1,1,0,0,0-.293-.707L16.293,3.293A1,1,0,0,0,15.586,3H4A1,1,0,0,0,3,4V20a1,1,0,0,0,1,1H20A1,1,0,0,0,21,20ZM9,8h4a1,1,0,0,1,0,2H9A1,1,0,0,1,9,8Zm7,11H8V15a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z"/></svg>`
+
+const CancelIcon =  `<svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>`
+
+const AddIcon = `<svg focusable viewBox="0 0 24 24" aria-hidden="true xmlns="http://www.w3.org/2000/svg"><path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>`
 
 const eventsAPIs = (function () {
   const API_URL = "http://localhost:3000/events";
@@ -79,9 +89,9 @@ class EventsView {
   constructor() {
     this.newEventForm = document.querySelector("#new-events-form");
     this.newEventFormDisplay = document.querySelector(
-      "#new-events-form__tfoot"
+      "#add-events-form__tfoot"
     );
-    this.addEventButton = document.querySelector("#add-event__button");
+    this.addEventButton = document.querySelector("#toggle-add-event__button");
     this.cancelAddEventButton = document.querySelector(
       "#cancel-add-event__button"
     );
@@ -93,6 +103,9 @@ class EventsView {
   }
 
   init_view() {
+    document.querySelector("#add-event__button").innerHTML = SaveIcon; 
+    document.querySelector("#cancel-add-event__button").innerHTML = CancelIcon; 
+
     this.newEventFormDisplay.style.visibility = "collapse";
     this.cancelAddEventButton.addEventListener("click", () => {
       this.newEventFormDisplay.style.visibility = "collapse";
@@ -143,11 +156,10 @@ class EventsView {
     </td>
     <td>
       <button type="submit" class="update-event__button">
-      Update
-      </button></td>
-    <td>
+      ${SaveIcon}
+      </button>
       <button type="button" class="cancel-edit-event__button">
-        Cancel Edit
+        ${CancelIcon}
       </button>
     </td>
     </form>
@@ -174,8 +186,14 @@ class EventsView {
           <th scope="row" id=${event.id}-eventName__td>${event.eventName}</th>
           <td id=${event.id}-startDate__td>${event.startDate}</td>
           <td id=${event.id}-endDate__td>${event.endDate}</td>
-          <td><button class="edit-event__button">Edit </button></td>
-          <td><button class="event__del-btn">Delete</button></td>
+          <td>
+            <button class="edit-event__button">
+             ${EditIcon}
+            </button>
+            <button class="delete-event__button">
+              ${DeleteIcon}
+            </button>
+          </td>
           `;
     return eventElement;
   }
@@ -209,7 +227,7 @@ class EventsController {
     //event delegation
     this.view.eventList.addEventListener("click", async (e) => {
       const elem = e.target;
-      if (elem.classList.contains("event__del-btn")) {
+      if (elem.classList.contains("delete-event__button")) {
         const eventElem = elem.parentElement.parentElement;
         const deleteId = eventElem.id;
         await eventsAPIs.deleteEvent(deleteId);
@@ -221,6 +239,7 @@ class EventsController {
 
   setUpEditEventToggle() {
     this.view.eventList.addEventListener("click", async (e) => {
+      console.log("clicked ", e.target)
       const elem = e.target;
       if (elem.classList.contains("cancel-edit-event__button")) {
         const eventId = elem.parentElement.parentElement.id;
